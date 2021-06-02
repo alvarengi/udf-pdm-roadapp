c5678c1da5fa18b870858b1c2deec173a4fc8331
none <iveensz_@live.com>
2021-06-01 22:18:34 -0300

    refactor: transforma CardOcorrencia em componente funcional

--- components/CardOcorrencia.js
+++ components/CardOcorrencia.js
@@ -2,39 +2,15 @@ import React, { Component } from "react";
 import { View, Text } from "react-native";
 import Styles from "../styles";
 
-class CardOcorrencia extends Component {
-	constructor(props) {
-		super(props);
-		const { Tipo, Local, Horario, Icone } = this.props;
-		this.state = {
-			Tipo,
-			Local,
-			Horario,
-			Icone,
-		};
-	}
-
-	render() {
-		return (
-			<View style={Styles.CardOcorrencia.Container}>
-				<View style={Styles.CardOcorrencia.Icone}>
-					{this.state.Icone}
-					{/* <i class="i isomething"></i> */}
-				</View>
-				<View style={Styles.CardOcorrencia.Descricao}>
-					<Text style={Styles.CardOcorrencia.Texto}>
-						Tipo: {this.state.Tipo}
-					</Text>
-					<Text style={Styles.CardOcorrencia.Texto}>
-						Local: {this.state.Local}
-					</Text>
-					<Text style={Styles.CardOcorrencia.Texto}>
-						Horário: {this.state.Horario}
-					</Text>
-				</View>
-			</View>
-		);
-	}
-}
+const CardOcorrencia = ({ Tipo, Local, Horario, Icone }) => (
+	<View style={Styles.CardOcorrencia.Container}>
+		<View style={Styles.CardOcorrencia.Icone}>{Icone}</View>
+		<View style={Styles.CardOcorrencia.Descricao}>
+			<Text style={Styles.CardOcorrencia.Texto}>Tipo: {Tipo}</Text>
+			<Text style={Styles.CardOcorrencia.Texto}>Local: {Local}</Text>
+			<Text style={Styles.CardOcorrencia.Texto}>Horário: {Horario}</Text>
+		</View>
+	</View>
+);
 
 export default CardOcorrencia;
